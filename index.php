9abbf140e629839b577ff803b86c5569561942b8
Guilherme Yamasaki <guilhermeyyamasaki1@gmail.com>
2022-04-30 11:16:16 -0300

    Main

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li class="logo"><img src="nagito.png"></li>
+            <li><a href="#"><i class="fa fa-home"></i>&nbsp; Usuário</a></li>
+            <li><a href="#"><i class="fa fa-book"></i>&nbsp; Configurações</a></li>
+            <li><a href="#"><i class="fa fa-users"></i>&nbsp; Menu</a></li>
+            <li><a href="#"><i class="fa fa-picture-o"></i>&nbsp; Pictures</a></li>
+            <li><a href="#"><i class="fa fa-phone"></i>&nbsp; About</a></li>
+        </ul>
+    </nav>
+    <div class="wrapper">
+        <div class="section">
+            <div class="box-area">
+                <h2>Home</h2>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
