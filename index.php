ded08f31a108b9a7c4d0efd53382637598a4cd70
Guilherme Yamasaki <guilhermeyyamasaki1@gmail.com>
2022-04-30 12:38:30 -0300

    Teste g

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" rel="stylesheet">
-    <title>Document</title>
+    <link rel="stylesheet" href="resources/style.css">
+    <title>Flash Cards</title>
 </head>
 <body>
     <nav>
         <ul>
-            <li class="logo"><img src="nagito.png"></li>
-            <li><a href="#"><i class="fa fa-home"></i>&nbsp; Usuário</a></li>
-            <li><a href="#"><i class="fa fa-book"></i>&nbsp; Configurações</a></li>
-            <li><a href="#"><i class="fa fa-users"></i>&nbsp; Menu</a></li>
-            <li><a href="#"><i class="fa fa-picture-o"></i>&nbsp; Pictures</a></li>
-            <li><a href="#"><i class="fa fa-phone"></i>&nbsp; About</a></li>
+            <li class="logo"><img src="stickman.png"></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="decklist.php">Decks</a></li>
+            <li><a href="#">Friends</a></li>
+            <li><a href="#">Settings</a></li>
+            <li><a href="about.php">About</a></li>
         </ul>
     </nav>
     <div class="wrapper">
