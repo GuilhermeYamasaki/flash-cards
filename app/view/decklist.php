5a803a14c183502db4dc936dd21ed126b1ab6fb2
Guilherme Yamasaki <guilhermeyyamasaki1@gmail.com>
2022-05-01 14:21:29 -0300

    teste

---
+++ app/view/decklist.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Deck list</title>
+</head>
+<body>
+    <h1>Criar deck</h1>
+    <form action="create-deck.php" method = "POST">
+        <input type="text" placeholder = "Nome do deck" name = "nameDeck" id = "name">
+        <button>Criar</button>
+    </form>
+    <h2>Seus decks</h2>
+    <ul>
+        <li>
+            <form action="#" method = "">
+                
+            </form>
+        </li>
+    </ul>
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,26 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="resources/style.css">
+    <link rel="stylesheet" href="app/css/style.css">
     <title>Flash Cards</title>
 </head>
 <body>
     <nav>
         <ul>
-            <li class="logo"><img src="stickman.png"></li>
+            <li class="logo"><img src="app/css/stickman.png"></li>
             <li><a href="#">Home</a></li>
-            <li><a href="decklist.php">Decks</a></li>
+            <li><a href="app/decklist.php">Decks</a></li>
             <li><a href="#">Friends</a></li>
             <li><a href="#">Settings</a></li>
             <li><a href="about.php">About</a></li>
         </ul>
     </nav>
-    <div class="wrapper">
-        <div class="section">
-            <div class="box-area">
-                <h2>Home</h2>
-            </div>
-        </div>
-    </div>
 </body>
 </html>
